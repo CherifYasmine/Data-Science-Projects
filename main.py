56937e46510e3a2333646ac422dcf0e72d6219c1
CherifYasmine <yasmine.chirif@insat.u-carthage.tn>
2022-03-21 15:53:48 +0100

    build the model

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import glob
 import shutil
 from sklearn.model_selection import train_test_split
 from utils import split_data, order_test_set
-
+from models import signs_model
 
 if __name__ == "__main__":
     if False:
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         path_to_save_train = "C:\\Users\\hp\Downloads\\archive\\training_data\\train"
         path_to_save_val = "C:\\Users\\hp\Downloads\\archive\\training_data\\val"
         split_data(path_to_data, path_to_save_train, path_to_save_val)
-    path_to_images = "C:\\Users\\hp\Downloads\\archive\\Test"
-    path_to_csv = "C:\\Users\\hp\Downloads\\archive\\Test.csv"
-    order_test_set(path_to_images, path_to_csv)
+        path_to_images = "C:\\Users\\hp\Downloads\\archive\\Test"
+        path_to_csv = "C:\\Users\\hp\Downloads\\archive\\Test.csv"
+        order_test_set(path_to_images, path_to_csv)
